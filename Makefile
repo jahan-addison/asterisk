e082d66dbba968342e0a8bd3992c1d5d39889326
Jahan Addison <info@jacata.me>
2026-05-26 00:33:29 -0400

    chore: initial work on parallax backgrounds

---
+++ Makefile
@@ -0,0 +1,5 @@
+PULSE2D_ROOT = ext/pulse2d
+GAME_SRCS    = src/asterisk.cc
+GAME_NAME    = asterisk
+
+include $(PULSE2D_ROOT)/Makefile.teensy
--- src/asterisk.cc
+++ src/asterisk.cc
@@ -2,71 +2,48 @@
 #include PULSE2D_HEADER
 #include PULSE2D_GRAPHICS
 
+// #include "../include/dust-bg.h"
+#include "../include/nebula-bg.h"
+// #include "../include/planet-bg.h"
+// #include "../include/stars-bg.h"
+
 PULSE2D_START_PULSE();
 
 PULSE2D_ENABLE_SEESAW_GAMEPAD();
 
-PULSE2D_DEFINE_SCENE(Sample_Level, 2, 3);
+PULSE2D_DEFINE_SCENE(Level_One, 1, 1);
 
-PULSE2D_GAME_SCENES(Sample_Level);
+PULSE2D_GAME_SCENES(Level_One);
 
-PULSE2D_DEFINE bool exploded = false;
-PULSE2D_DEFINE bool fired = false;
-
-PULSE2D_ON_GAMESCENE_START(Sample_Level)
+PULSE2D_ON_GAMESCENE_START(Level_One)
 {
-    PULSE2D_SPAWN_STATIC_BODY("planet",
-        {
-            .position = { 3.5f, 0.0f },
-              .width = { 1.0f, 1.0f }
-    });
+    PULSE2D_SET_SPRITE_FLASH(sprite_nebula, bg_1, 320, 240);
+    //  PULSE2D_SET_SPRITE_FLASH(sprite_stars, bg_2, 320, 240);
+    //  PULSE2D_SET_SPRITE_FLASH(sprite_planets, bg_3, 320, 240);
+    //   PULSE2D_SET_SPRITE_FLASH(sprite_dust, bg_4, 320, 240);
 
-    PULSE2D_SPAWN_BODY("spell",
-        {
-            .position = { -3.5f, 2.5111f },
-            .velocity = { 0.0f,  0.0f    },
-            .width = { 1.0f,  0.5f    },
-            .mass = 1.0f
-    });
-    PULSE2D_SET_SPRITE(planet_sprite, "planet.bin", 96, 96);
-    PULSE2D_SET_SPRITE(spell_sprite, "spell.bin", 64, 36);
-    PULSE2D_SET_SPRITE(explode_sprite, "explosion.bin", 96, 96);
+    // PULSE2D_ADD_SCROLLING_LAYER(sprite_nebula, 320.0f, 10.0f);
+    //  PULSE2D_ADD_SCROLLING_LAYER(sprite_stars, 320.0f, 3.0f);
+    //  PULSE2D_ADD_SCROLLING_LAYER(sprite_planets, 320.0f, 25.0f);
+    //   PULSE2D_ADD_SCROLLING_LAYER(sprite_dust, 320.0f, 65.0f);
+
+    // PULSE2D_SET_SPRITE(sprite_ship, "/ship.bin", 64, 64);
+
+    // Spawn the player's physics body
+    // Note: Locked to left side, middle of screen
+    // PULSE2D_SPAWN_DYNAMIC_BODY(player,
+    //     {
+    //         .position = { 200.0f, 540.0f },
+    //         .width = { 64.0f,  64.0f  },
+    //         .mass = 1.0f
+    // });
 }
 
-PULSE2D_ON_GAMESCENE(Sample_Level)
+PULSE2D_ON_GAMESCENE(Level_One)
 {
-    PULSE2D_TICK_WORLD(Sample_Level);
+    PULSE2D_TICK_WORLD(Level_One);
     PULSE2D_POLL_SEESAW_GAMEPAD();
-    auto& spell = PULSE2D_GET_BODY("spell");
-
-    PULSE2D_ON_COLLISION()
-    {
-        if (!exploded) {
-            exploded = true;
-            spell.velocity = { 0.0f, 0.0f };
-        }
-    }
-
-    if (!fired)
-        SEESAW_ARCADE_DIRECTIONAL_MOVEMENT_INVERTED("spell", 5.22f);
-
-    if (!fired and SEESAW_BUTTON_INPUT(SEESAW_A)) {
-        spell.velocity = { 12.555f, 0.0f };
-        fired = true;
-    }
-
-    // reset?
-    if (spell.position.x > 5.5f or SEESAW_BUTTON_INPUT(SEESAW_START)) {
-        fired = false;
-        PULSE2D_SET_SCENE(Sample_Level);
-    }
-
-    if (exploded)
-        PULSE2D_DRAW("planet", explode_sprite);
-    else
-        PULSE2D_DRAW("planet", planet_sprite);
-
-    PULSE2D_DRAW("spell", spell_sprite, 3.111f);
+    PULSE2D_RENDER_BACKGROUNDS();
     PULSE2D_RENDER(active_scene);
 }
 
@@ -74,13 +51,13 @@ PULSE2D_ON_GAMESTART()
 {
     Serial.begin(115200);
 
-    // PULSE2D_POLL_SERIAL_CONNECTION();
+    PULSE2D_POLL_SERIAL_CONNECTION();
 
     PULSE2D_INIT(0.0f, 0.0f, 10);
 
     PULSE2D_START_SEESAW_GAMEPAD();
 
-    PULSE2D_SET_SCENE(Sample_Level);
+    PULSE2D_SET_SCENE(Level_One);
 }
 
 PULSE2D_ON_GAMELOOP()
