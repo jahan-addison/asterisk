8730e68abbc8f72ce5aaf392383b84b930f22ca7
Jahan Addison <info@jacata.me>
2026-06-07 19:32:07 -0400

    feat: laser gun sprite and animation physics

--- src/asterisk.cc
+++ src/asterisk.cc
@@ -11,9 +11,12 @@ PULSE2D_START_PULSE();
 
 PULSE2D_ENABLE_SEESAW_GAMEPAD();
 
-PULSE2D_DEFINE_SCENE(Level_One, 1, 5);
+// @TODO: Better error handling on malformed sizing
+PULSE2D_DEFINE_SCENE(Level_One, 2, 6);
 
 PULSE2D_GAME_SCENES(Level_One);
+
+PULSE2D_DEFINE bool gun_fired = false;
 
 PULSE2D_ON_GAMESCENE_START(Level_One)
 {
@@ -21,15 +24,31 @@ PULSE2D_ON_GAMESCENE_START(Level_One)
     // Ship setup //
     ////////////////
 
-    PULSE2D_SPAWN_BODY("ship",
+    PULSE2D_SPAWN_STATIC_BODY("ship",
         {
             .position = { -4.32f, 2.5111f },
             .velocity = { 0.0f,   0.0f    },
-            .width = { 1.0f,   0.5f    },
+            .width = { 1.0f,   1.0f    },
+            .friction = 0.0f,
             .mass = 1.0f
     });
 
     PULSE2D_SET_SPRITE(ship_sprite, "ship_1.bin", 44, 39);
+
+    PULSE2D_SET_SPRITE(laser_sprite, "fire_shot_2_2.bin", 35, 4);
+
+    ////////////////
+    // Ammo setup //
+    ////////////////
+
+    PULSE2D_SPAWN_STATIC_BODY("laser",
+        {
+            .position = { -10.0f, 0.0f },
+            .velocity = { 0.0f,   0.0f },
+            .width = { 1.0f,   0.5f },
+            .friction = 0.0f,
+            .mass = 1.0f
+    });
 
     //////////////////////
     // Background setup //
@@ -51,10 +70,32 @@ PULSE2D_ON_GAMESCENE(Level_One)
     PULSE2D_TICK_WORLD(Level_One);
     PULSE2D_POLL_SEESAW_GAMEPAD();
 
-    SEESAW_ARCADE_DIRECTIONAL_MOVEMENT_INVERTED("ship", 7.22f, true);
+    auto& ship = PULSE2D_GET_BODY("ship");
+    auto& laser = PULSE2D_GET_BODY("laser");
+
+    // @TODO: Prevent leaving the screen
+    SEESAW_ARCADE_DIRECTIONAL_MOVEMENT_INVERTED("ship", 12.55f, true);
+
+    if (SEESAW_BUTTON_INPUT(SEESAW_A)) {
+        if (!gun_fired) {
+            gun_fired = true;
+            laser.position = { ship.position.x, ship.position.y };
+            laser.velocity = { 25.0f, 0.0f };
+        }
+    }
 
     PULSE2D_RENDER_BACKGROUNDS();
     PULSE2D_DRAW("ship", ship_sprite);
+
+    if (gun_fired) {
+        // laser shot went off the screen, reset it
+        if (laser.position.x > 6.67f) {
+            gun_fired = false;
+            laser.position = { -10.0f, 0.0f };
+            laser.velocity = { 0.0f, 0.0f };
+        }
+        PULSE2D_DRAW("laser", laser_sprite);
+    }
 
     PULSE2D_RENDER(active_scene);
 }
