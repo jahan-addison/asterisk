98c4977686e3dd1e65640c0c7ffb03ee24944487
Jahan Addison <info@jacata.me>
2026-06-30 21:31:46 -0400

    asteroid state machine improvements and helpers, asset alignment

--- include/asteroids.h
+++ include/asteroids.h
@@ -5,7 +5,9 @@
 #include PULSE2D_GRAPHICS
 
 #include <etl/array.h>
+#include <etl/map.h>
 
+#include <concepts>
 #include <variant>
 
 namespace asteroid {
@@ -32,9 +34,7 @@ PULSE2D_INLINE pulse2d_math::Vec2 get_size_by_pixel(Size size,
     return { w - co_e, h - co_e };
 }
 
-// clang-format off
 // Events
-
 struct Spawn_Event
 {
     Size size;
@@ -46,13 +46,12 @@ struct Crash_Event
 {};
 
 // States
-
 struct Live_State
 {
     p_ui8 hits{ 0 };
     Size current_size = Size::E;
-    p_ui8 total_sizes{0};
-    p_ui8 shrink{0};
+    p_ui8 total_sizes{ 0 };
+    p_ui8 shrink{ 0 };
 };
 
 struct Crashed_State
@@ -62,23 +61,17 @@ struct Erased_State
 struct Spawned_State
 {};
 
-// Manager
-
-using Event = std::variant<Spawn_Event,
-    Impact_Event,
-    Crash_Event>;
-
-using State = std::variant<Spawned_State,
-    Erased_State,
-    Crashed_State,
-    Live_State>;
-
-// clang-format on
-
-class Asteroid_SM
+class Asteroid_Controller
 {
   public:
-    Asteroid_SM() = default;
+    Asteroid_Controller() = default;
+
+  public:
+    using Event = std::variant<Spawn_Event, Impact_Event, Crash_Event>;
+    using State =
+        std::variant<Spawned_State, Erased_State, Crashed_State, Live_State>;
+
+  public:
     void dispatch(Event const& event)
     {
         current_state = std::visit(
@@ -99,8 +92,6 @@ class Asteroid_SM
 
                 [](Live_State& s, Impact_Event const&) -> State {
                     if (s.hits >= 2) {
-                        if (s.total_sizes == 1)
-                            return Erased_State{};
                         auto current = static_cast<int>(s.current_size);
                         if (current > 0 and current <= 4)
                             s.current_size = static_cast<Size>(current + 1);
@@ -126,7 +117,6 @@ class Asteroid_SM
     }
 
     PULSE2D_INLINE State get_current_state() const { return current_state; }
-    PULSE2D_INLINE void start() { current_state = Spawned_State{}; }
     PULSE2D_INLINE void reset() { current_state = Spawned_State{}; }
 
   private:
@@ -134,15 +124,47 @@ class Asteroid_SM
 };
 
 template<size_t N = 4>
-struct Asteroid_Manager
+class Asteroid_Manager
 {
+  public:
     Asteroid_Manager() = default;
-    etl::array<Asteroid_SM, N> objects;
-    void reset_all()
+    PULSE2D_INLINE Asteroid_Manager& add(const char* name,
+        Size size,
+        p_ui8 total)
     {
-        for (auto& v : objects)
-            v.reset();
+        objects.at(index).dispatch(asteroid::Spawn_Event{ size, total });
+        mapper[name] = index++;
+        return *this;
     }
+    PULSE2D_INLINE Asteroid_Controller& get(const char* name)
+    {
+        return objects.at(mapper.at(name));
+    }
+    PULSE2D_INLINE void reset_all()
+    {
+        for (auto& obj : objects)
+            obj.reset();
+    }
+
+    template<typename State_Type, typename Func>
+    requires std::invocable<Func, const State_Type&>
+    bool execute_if_state(const char* name, Func&& action)
+    {
+        auto const& current_state =
+            objects.at(mapper.at(name)).get_current_state();
+
+        if (auto const* state_ptr = std::get_if<State_Type>(&current_state)) {
+            std::forward<Func>(action)(*state_ptr);
+            return true;
+        }
+
+        return false;
+    }
+
+  private:
+    etl::array<Asteroid_Controller, N> objects;
+    etl::map<const char*, std::size_t, N, pulse2d::CStrLess> mapper;
+    size_t index{ 0 };
 };
 
 } // namespace asteriods
--- src/asterisk.cc
+++ src/asterisk.cc
@@ -26,6 +26,8 @@ PULSE_DEFINE int current_asteroid = 0;
 
 PULSE_DEFINE asteroid::Asteroid_Manager<4> lvl_1_asteroids_{};
 
+// @TODO: Split out level 1 code
+
 /**
  * @brief
  * Set the player ship object
@@ -99,6 +101,23 @@ void set_static_walls()
 
 /**
  * @brief
+ * Dispatch impact event on laser collision with an asteroid.
+ *
+ * @scope: PULSE_ON_GAMESCENE_START(Level_One)
+ */
+PULSE2D_INLINE void kinematic_laser_collided_asteriod(
+    pulse2d_body* laser_object,
+    const char* name)
+{
+    lvl_1_asteroids_.get(name).dispatch(asteroid::Impact_Event{});
+    auto coords = get_body_coordinates(laser_object);
+    asterisk.play_vfx(
+        "laser_collision", to_int16(coords.x + 12), to_int16(coords.y - 12));
+    asterisk.despawn("laser_gun", laser_object);
+}
+
+/**
+ * @brief
  * First level Scene Start
  *
  * @scope: Level_One
@@ -110,26 +129,26 @@ PULSE_ON_GAMESCENE_START(Level_One)
     set_player_ship();
 
     // 1 - Size: 45 45
-    asterisk.set_sprite("meteor_1m_sprite", "meteors/meteor_1_m.bin");
-    asterisk.set_sprite("meteor_1s_sprite", "meteors/meteor_1_s.bin");
+    asterisk.set_sprite("asteroid_1m_sprite", "asteroids/asteroid_1_m.bin");
+    asterisk.set_sprite("asteroid_1s_sprite", "asteroids/asteroid_1_s.bin");
     // 2 - Size: 25 22
-    asterisk.set_sprite("meteor_5s_sprite", "meteors/meteor_5_s.bin");
+    asterisk.set_sprite("asteroid_5s_sprite", "asteroids/asteroid_5_s.bin");
     // 3 - Size: 62 65
-    asterisk.set_sprite("meteor_6l_sprite", "meteors/meteor_6_l.bin");
+    asterisk.set_sprite("asteroid_6l_sprite", "asteroids/asteroid_6_l.bin");
     // 4 - Size: 65 58
-    asterisk.set_sprite("meteor_2l_sprite", "meteors/meteor_6_l.bin");
-    asterisk.set_sprite("meteor_2m_sprite", "meteors/meteor_6_m.bin");
+    asterisk.set_sprite("asteroid_2l_sprite", "asteroids/asteroid_6_l.bin");
+    asterisk.set_sprite("asteroid_2m_sprite", "asteroids/asteroid_6_m.bin");
 
-    asterisk.set_dynamic_body("meteor_object_1",
+    asterisk.set_dynamic_body("asteroid_object_1",
         {
-            .position = { 6.0f,    -2.5f },
-            .velocity = { -7.555f, 0.0f  },
+            .position = { 6.0f,                             pulse2d_math::trng_random(-2.5f, 2.5f) },
+            .velocity = { pulse2d_math::trng_random(-10.0f, -7.5f),                          0.0f  },
             .width = px_to_units(65.0f, 65.0f),
             .mass = 1.0f,
             .is_sensor = true
     });
 
-    asterisk.set_dynamic_body("meteor_object_2",
+    asterisk.set_dynamic_body("asteroid_object_2",
         {
             .position = { 6.0f,     1.51f },
             .velocity = { -10.555f, 0.0f  },
@@ -138,43 +157,28 @@ PULSE_ON_GAMESCENE_START(Level_One)
             .is_sensor = true
     });
 
-    asterisk.set_dynamic_body("meteor_object_3",
+    asterisk.set_dynamic_body("asteroid_object_3",
         {
             .position = { 6.0f,    1.51f },
             .velocity = { -7.555f, 0.0f  },
             .width = px_to_units(62.0f, 65.0f),
-            .mass = 2.0f,
+            .mass = 1.0f,
             .is_sensor = true
     });
 
-    asterisk.set_dynamic_body("meteor_object_4",
+    asterisk.set_dynamic_body("asteroid_object_4",
         {
             .position = { 6.0f,    1.51f },
             .velocity = { -6.555f, 0.0f  },
             .width = px_to_units(65.0f, 58.0f),
-            .mass = 2.0f,
+            .mass = 1.0f,
             .is_sensor = true
     });
 
-    lvl_1_asteroids_.objects.at(0).dispatch(
-        asteroid::Spawn_Event{ asteroid::Size::M, 2 });
-    lvl_1_asteroids_.objects.at(1).dispatch(
-        asteroid::Spawn_Event{ asteroid::Size::S, 1 });
-    lvl_1_asteroids_.objects.at(2).dispatch(
-        asteroid::Spawn_Event{ asteroid::Size::L, 1 });
-    lvl_1_asteroids_.objects.at(3).dispatch(
-        asteroid::Spawn_Event{ asteroid::Size::L, 2 });
-}
-
-PULSE2D_INLINE void kinematic_laser_collided_asteriod(
-    pulse2d_body* laser_object,
-    size_t index)
-{
-    lvl_1_asteroids_.objects[index].dispatch(asteroid::Impact_Event{});
-    auto coords = get_body_coordinates(laser_object);
-    asterisk.play_vfx(
-        "laser_collision", to_int16(coords.x + 12), to_int16(coords.y - 12));
-    asterisk.despawn("laser_gun", laser_object);
+    lvl_1_asteroids_.add("asteroid_object_1", asteroid::Size::M, 2)
+        .add("asteroid_object_2", asteroid::Size::S, 1)
+        .add("asteroid_object_3", asteroid::Size::L, 1)
+        .add("asteroid_object_4", asteroid::Size::L, 2);
 }
 
 PULSE_ON_GAMESCENE(Level_One)
@@ -210,121 +214,124 @@ PULSE_ON_GAMESCENE(Level_One)
 
     asterisk.draw("ship_object", "ship_sprite");
 
-    auto& as_1 = asterisk.get_body("meteor_object_1");
-    auto& as_2 = asterisk.get_body("meteor_object_2");
-    auto& as_3 = asterisk.get_body("meteor_object_3");
-    auto& as_4 = asterisk.get_body("meteor_object_4");
-
-    const auto& as_1_state = lvl_1_asteroids_.objects.at(0).get_current_state();
-    const auto& as_2_state = lvl_1_asteroids_.objects.at(1).get_current_state();
-    const auto& as_3_state = lvl_1_asteroids_.objects.at(2).get_current_state();
-    const auto& as_4_state = lvl_1_asteroids_.objects.at(3).get_current_state();
+    auto& as_1 = asterisk.get_body("asteroid_object_1");
+    auto& as_2 = asterisk.get_body("asteroid_object_2");
+    auto& as_3 = asterisk.get_body("asteroid_object_3");
+    auto& as_4 = asterisk.get_body("asteroid_object_4");
 
     // @TODO: Reset with faster velocity, random order asteroid selection
     switch (current_asteroid) {
         case 0: {
-            std::visit(
-                pulse2d_util::overload{
-                    [&](asteroid::Spawned_State const&) {
-                        asterisk.draw("meteor_object_1", "meteor_1m_sprite");
-                    },
-                    [](asteroid::Crashed_State const&) {
-                        // game over
-                    },
-                    [&](asteroid::Erased_State const&) {
-                        as_1.set_width({ 0.0f, 0.0f });
-                        as_2.set_position(
-                            { 6.0f, pulse2d_math::trng_random(-2.5f, 2.5f) });
-                        current_asteroid++;
-                    },
-                    [&](asteroid::Live_State const& s) {
-                        if (s.current_size == asteroid::Size::M) {
-                            asterisk.draw(
-                                "meteor_object_1", "meteor_1m_sprite");
-                        }
-                        if (s.current_size == asteroid::Size::S) {
-                            as_1.set_width(asteroid::get_size_by_pixel(
-                                asteroid::Size::S, 45, 45));
-                            asterisk.draw(
-                                "meteor_object_1", "meteor_1s_sprite");
-                        }
-                    },
-                },
-                as_1_state);
+            lvl_1_asteroids_.execute_if_state<asteroid::Spawned_State>(
+                "asteroid_object_1", [&](asteroid::Spawned_State const&) {
+                    asterisk.draw("asteroid_object_1", "asteroid_1m_sprite");
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Crashed_State>(
+                "asteroid_object_1", [&](asteroid::Crashed_State const&) {
+                    // gameover
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Erased_State>(
+                "asteroid_object_1", [&](asteroid::Erased_State const&) {
+                    as_1.set_width({ 0.0f, 0.0f });
+                    as_2.set_position(
+                        { 6.0f, pulse2d_math::trng_random(-2.5f, 2.5f) });
+                    current_asteroid++;
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Live_State>(
+                "asteroid_object_1", [&](asteroid::Live_State const& s) {
+                    if (s.current_size == asteroid::Size::M) {
+                        asterisk.draw(
+                            "asteroid_object_1", "asteroid_1m_sprite");
+                    }
+                    if (s.current_size == asteroid::Size::S) {
+                        as_1.set_width(asteroid::get_size_by_pixel(
+                            asteroid::Size::S, 45, 45));
+                        asterisk.draw(
+                            "asteroid_object_1", "asteroid_1s_sprite");
+                    }
+                });
         } break;
         case 1: {
-            std::visit(
-                pulse2d_util::overload{
-                    [&](asteroid::Spawned_State const&) {
-                        asterisk.draw("meteor_object_2", "meteor_5s_sprite");
-                    },
-                    [](asteroid::Crashed_State const&) {
-                        // game over
-                    },
-                    [&](asteroid::Erased_State const&) {
-                        as_2.set_width({ 0.0f, 0.0f });
-                        as_3.set_position(
-                            { 6.0f, pulse2d_math::trng_random(-2.5f, 2.0f) });
-                        current_asteroid++;
-                    },
-                    [&](asteroid::Live_State const&) {
-                        asterisk.draw("meteor_object_2", "meteor_5s_sprite");
-                    },
-                },
-                as_2_state);
+            lvl_1_asteroids_.execute_if_state<asteroid::Spawned_State>(
+                "asteroid_object_2", [&](asteroid::Spawned_State const&) {
+                    asterisk.draw("asteroid_object_2", "asteroid_5s_sprite");
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Crashed_State>(
+                "asteroid_object_2", [&](asteroid::Crashed_State const&) {
+                    // gameover
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Erased_State>(
+                "asteroid_object_2", [&](asteroid::Erased_State const&) {
+                    as_2.set_width({ 0.0f, 0.0f });
+                    as_3.set_position(
+                        { 6.0f, pulse2d_math::trng_random(-2.5f, 2.5f) });
+                    current_asteroid++;
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Live_State>(
+                "asteroid_object_2", [&](asteroid::Live_State const& s) {
+                    if (s.current_size == asteroid::Size::M) {
+                        asterisk.draw(
+                            "asteroid_object_1", "asteroid_1m_sprite");
+                    }
+                    if (s.current_size == asteroid::Size::S) {
+                        asterisk.draw(
+                            "asteroid_object_2", "asteroid_5s_sprite");
+                    }
+                });
         } break;
         case 2: {
-            std::visit(
-                pulse2d_util::overload{
-                    [&](asteroid::Spawned_State const&) {
-                        asterisk.draw("meteor_object_3", "meteor_6l_sprite");
-                    },
-                    [](asteroid::Crashed_State const&) {
-                        // game over
-                    },
-                    [&](asteroid::Erased_State const&) {
-                        as_3.set_width({ 0.0f, 0.0f });
-                        as_4.set_position(
-                            { 6.0f, pulse2d_math::trng_random(-2.5f, 2.5f) });
-                        current_asteroid++;
-                    },
-                    [&](asteroid::Live_State const&) {
-                        asterisk.draw("meteor_object_3", "meteor_6l_sprite");
-                    },
-                },
-                as_3_state);
+            lvl_1_asteroids_.execute_if_state<asteroid::Spawned_State>(
+                "asteroid_object_3", [&](asteroid::Spawned_State const&) {
+                    asterisk.draw("asteroid_object_3", "asteroid_6l_sprite");
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Crashed_State>(
+                "asteroid_object_3", [&](asteroid::Crashed_State const&) {
+                    // gameover
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Erased_State>(
+                "asteroid_object_3", [&](asteroid::Erased_State const&) {
+                    as_3.set_width({ 0.0f, 0.0f });
+                    as_4.set_position(
+                        { 6.0f, pulse2d_math::trng_random(-2.5f, 2.5f) });
+                    current_asteroid++;
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Live_State>(
+                "asteroid_object_3", [&](asteroid::Live_State const&) {
+                    asterisk.draw("asteroid_object_3", "asteroid_6l_sprite");
+                });
         } break;
         case 3: {
-            std::visit(pulse2d_util::overload{
-                           [&](asteroid::Spawned_State const&) {
-                               asterisk.draw(
-                                   "meteor_object_4", "meteor_2l_sprite");
-                           },
-                           [](asteroid::Crashed_State const&) {
-                               // game over
-                           },
-                           [&](asteroid::Erased_State const&) {
-                               as_4.set_width({ 0.0f, 0.0f });
-                               current_asteroid = 0;
-                           },
-                           [&](asteroid::Live_State const& s) {
-                               if (s.current_size == asteroid::Size::L) {
-                                   asterisk.draw(
-                                       "meteor_object_4", "meteor_2l_sprite");
-                               }
-                               if (s.current_size == asteroid::Size::M) {
-                                   as_4.set_width(asteroid::get_size_by_pixel(
-                                       asteroid::Size::S, 65, 58));
-                                   asterisk.draw(
-                                       "meteor_object_4", "meteor_2m_sprite");
-                               }
-                           },
-                       },
-                as_4_state);
+
+            lvl_1_asteroids_.execute_if_state<asteroid::Spawned_State>(
+                "asteroid_object_4", [&](asteroid::Spawned_State const&) {
+                    asterisk.draw("asteroid_object_4", "asteroid_2l_sprite");
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Crashed_State>(
+                "asteroid_object_4", [&](asteroid::Crashed_State const&) {
+                    // gameover
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Erased_State>(
+                "asteroid_object_4", [&](asteroid::Erased_State const&) {
+                    as_4.set_width({ 0.0f, 0.0f });
+                    current_asteroid = 0;
+                });
+            lvl_1_asteroids_.execute_if_state<asteroid::Live_State>(
+                "asteroid_object_4", [&](asteroid::Live_State const& s) {
+                    if (s.current_size == asteroid::Size::L) {
+                        asterisk.draw(
+                            "asteroid_object_4", "asteroid_2l_sprite");
+                    }
+                    if (s.current_size == asteroid::Size::M) {
+                        as_4.set_width(asteroid::get_size_by_pixel(
+                            asteroid::Size::S, 65, 58));
+                        asterisk.draw(
+                            "asteroid_object_4", "asteroid_2m_sprite");
+                    }
+                });
         }
     }
 
-    // @TODO: on_collision_with_body(ship)
+    // @TODO: Explode ship on_collision_with_body(ship)
 
     asterisk.render_pool("laser_gun", [&](pulse2d_body* laser_object) {
         if (laser_object->position.x > 6.67f or
@@ -336,16 +343,20 @@ PULSE_ON_GAMESCENE(Level_One)
         asterisk.on_collision_with_body(
             laser_object, [&](pulse2d_body* collided_with) {
                 if (collided_with == &as_1 and current_asteroid == 0) {
-                    kinematic_laser_collided_asteriod(laser_object, 0);
+                    kinematic_laser_collided_asteriod(
+                        laser_object, "asteroid_object_1");
                 }
                 if (collided_with == &as_2 and current_asteroid == 1) {
-                    kinematic_laser_collided_asteriod(laser_object, 1);
+                    kinematic_laser_collided_asteriod(
+                        laser_object, "asteroid_object_2");
                 }
                 if (collided_with == &as_3 and current_asteroid == 2) {
-                    kinematic_laser_collided_asteriod(laser_object, 2);
+                    kinematic_laser_collided_asteriod(
+                        laser_object, "asteroid_object_3");
                 }
                 if (collided_with == &as_4 and current_asteroid == 3) {
-                    kinematic_laser_collided_asteriod(laser_object, 3);
+                    kinematic_laser_collided_asteriod(
+                        laser_object, "asteroid_object_4");
                 }
             });
     });
